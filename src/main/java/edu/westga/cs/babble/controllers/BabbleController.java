fe2d47dfe4753c8cb1001ea8aaf62cfe9f7ff894
Blair Pattison <blair.pattis@gmail.com>
2021-08-27 11:43:52 -0500

    added mouseevent and cell factory for populating played word listview

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -7,17 +7,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import edu.westga.cs.babble.model.EmptyTileBagException;
+import edu.westga.cs.babble.model.PlayedWord;
 import edu.westga.cs.babble.model.Tile;
 import edu.westga.cs.babble.model.TileBag;
+import edu.westga.cs.babble.model.TileNotInGroupException;
 import edu.westga.cs.babble.model.TileRack;
 import edu.westga.cs.babble.model.TileRackFullException;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Callback;
@@ -28,9 +34,11 @@ import javafx.util.Callback;
  */
 public class BabbleController implements Initializable {
 	@FXML private ListView<Tile> tilesDisplayed;
+	@FXML private ListView<Tile> yourWordDisplayed;
 	@FXML private Label testLabel;
 	private TileRack tiles;
 	private TileBag tileBag;
+	private PlayedWord playedTiles;
 
 
 	/**
@@ -40,14 +48,16 @@ public class BabbleController implements Initializable {
 	 */
 	public BabbleController() throws TileRackFullException, EmptyTileBagException {
 	this.tilesDisplayed = new ListView<>();
+	this.yourWordDisplayed = new ListView<>();
 	this.testLabel = new Label();
 	this.tileBag = new TileBag();
 	this.tiles = new TileRack();
+	this.playedTiles = new PlayedWord();
 	int tilesNeeded = 0;
 	do {
 		this.tiles.append(this.tileBag.drawTile());
 		tilesNeeded++;
-	} while (this.tiles.getNumberOfTilesNeeded() > tilesNeeded);
+	} while (tilesNeeded < 7);
 	
 	}
 
@@ -69,6 +79,9 @@ public class BabbleController implements Initializable {
 					@Override
 					protected void updateItem(Tile t, boolean bln) {
 						super.updateItem(t, bln);
+						if(t == null) {
+							setText(null);
+						}
 						if (t != null) {
 							setText(String.valueOf(t.getLetter()));
 						}
@@ -79,7 +92,48 @@ public class BabbleController implements Initializable {
 				return cell;
 			}
 		});
+	      
+		 yourWordDisplayed.setItems(playedTiles.tiles());
+		 
+        this.tilesDisplayed.setOnMousePressed(new EventHandler<MouseEvent>() {
+    	    public void handle(MouseEvent me) {
+    	    	playedTiles.append(tilesDisplayed.getSelectionModel().selectedItemProperty().get());
+    	    	try {
+					tiles.remove(tilesDisplayed.getSelectionModel().selectedItemProperty().get());
+				} catch (TileNotInGroupException e) {
+				
+					e.printStackTrace();
+				}
+        	   
+    	    }
+    	});
 
+		this.yourWordDisplayed.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+
+			@Override
+			public ListCell<Tile> call(ListView<Tile> arg0) {
+
+				ListCell<Tile> cell = new ListCell<Tile>() {
+
+					@Override
+					protected void updateItem(Tile t, boolean bln) {
+						super.updateItem(t, bln);
+						if(t == null) {
+							setText(null);
+						}
+						if (t != null) {
+							setText(String.valueOf(t.getLetter()));
+						}
+					}
+
+				};
+
+				return cell;
+			}
+		});
+	
+	    
+	
 	}
 	
 	}
