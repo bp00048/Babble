7782db2655e6bdae251e4c45e9fdbee4dc63928c
Blair Pattison <blair.pattis@gmail.com>
2021-08-29 11:43:42 -0500

    refactored listeners and made helper methods to make code more readable

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -4,7 +4,6 @@
 package edu.westga.cs.babble.controllers;
 
 import java.net.URL;
-import java.util.Optional;
 import java.util.ResourceBundle;
 
 import edu.westga.cs.babble.model.EmptyTileBagException;
@@ -15,30 +14,24 @@ import edu.westga.cs.babble.model.TileNotInGroupException;
 import edu.westga.cs.babble.model.TileRack;
 import edu.westga.cs.babble.model.TileRackFullException;
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.Property;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 import javafx.util.converter.NumberStringConverter;
 
 /**
- * @author windy
+ * @author Blair Pattison
+ * @version 08/29/2021
  *
  */
 public class BabbleController implements Initializable {
@@ -75,134 +68,103 @@ public class BabbleController implements Initializable {
 		this.resetButton = new Button();
 		this.playWordButton = new Button();
 		this.wordChecker = new WordDictionary();
-
 		this.fillTiles();
 
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
-		ObservableList<Tile> data = FXCollections.observableList(tiles.tiles());
-		this.tilesDisplayed.setItems(data);
-
-		this.tilesDisplayed.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
-
-			@Override
-			public ListCell<Tile> call(ListView<Tile> arg0) {
-
-				ListCell<Tile> cell = new ListCell<Tile>() {
-
-					@Override
-					protected void updateItem(Tile t, boolean bln) {
-						super.updateItem(t, bln);
-						if (t == null) {
-							setText(null);
-						}
-						if (t != null) {
-							setText(String.valueOf(t.getLetter()));
-						}
-					}
-
-				};
-
-				return cell;
-			}
-		});
-
-		yourWordDisplayed.setItems(playedTiles.tiles());
-
-		this.tilesDisplayed.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent me) {
-				playedTiles.append(tilesDisplayed.getSelectionModel().selectedItemProperty().get());
-				try {
-					tiles.remove(tilesDisplayed.getSelectionModel().selectedItemProperty().get());
-				} catch (TileNotInGroupException e) {
-					return;
-				}
-				tilesDisplayed.getSelectionModel().clearSelection();
-			}
-		});
-
-		this.yourWordDisplayed.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
-
-			@Override
-			public ListCell<Tile> call(ListView<Tile> arg0) {
-
-				ListCell<Tile> cell = new ListCell<Tile>() {
-
-					@Override
-					protected void updateItem(Tile t, boolean bln) {
-						super.updateItem(t, bln);
-						if (t == null) {
-							setText(null);
-						}
-						if (t != null) {
-							setText(String.valueOf(t.getLetter()));
-						}
-					}
-
-				};
-
-				return cell;
-			}
-		});
-
-		this.yourWordDisplayed.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent me) {
-				tiles.append(yourWordDisplayed.getSelectionModel().selectedItemProperty().get());
-				try {
-					playedTiles.remove(yourWordDisplayed.getSelectionModel().selectedItemProperty().get());
-				} catch (TileNotInGroupException e) {
-					return;
-				}
-				yourWordDisplayed.getSelectionModel().clearSelection();
-				tilesDisplayed.setItems(tiles.tiles());
-			}
-		});
-
-		this.resetButton.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent me) {
-				for (Tile current : playedTiles.tiles()) {
-					tiles.append(current);
-				}
-				tilesDisplayed.setItems(tiles.tiles());
-				playedTiles.clear();
-			}
-		});
-
-		this.playWordButton.setOnMousePressed(new EventHandler<MouseEvent>() {
-			public void handle(MouseEvent me) {
-				String currentHand = playedTiles.getHand();
-				if (wordChecker.isValidWord(currentHand)) {
-					runningScore += playedTiles.getScore();
-					playedTiles.clear();
-					IntegerProperty integerProperty = new SimpleIntegerProperty(runningScore);
-					scoreField.textProperty().bindBidirectional(integerProperty, new NumberStringConverter());
-					int tilesNeeded = tiles.getNumberOfTilesNeeded();
-					do {
-						try {
-							tiles.append(tileBag.drawTile());
-						} catch (TileRackFullException | EmptyTileBagException e) {
-							e.printStackTrace();
-						}
-						tilesNeeded--;
-					} while (tilesNeeded != 0);
-					tilesDisplayed.setItems(tiles.tiles());
-
-				} else {
-					Alert a = new Alert(AlertType.INFORMATION);
-
-					a.setHeaderText("Invalid word");
-
-					a.show();
-
-				}
-			}
-		});
-
+	this.buildYourTilesDisplayed();
+	this.buildGameTilesDisplayed();
+		 
+		this.tilesDisplayed.setOnMousePressed(new GameTileSelectedListener());
+		this.yourWordDisplayed.setOnMousePressed(new YourTileSelectedListener());
+		this.resetButton.setOnMousePressed(new ResetListener());
+		this.playWordButton.setOnMousePressed(new PlayWordListener());
 	}
 
+	public class TileListCell extends ListCell<Tile> {
+		 public TileListCell() {    }
+		 @Override
+		protected void updateItem(Tile t, boolean bln) {
+			super.updateItem(t, bln);
+			if (t == null) {
+				setText(null);
+			}
+			if (t != null) {
+				setText(String.valueOf(t.getLetter()));
+				setAccessibleText(String.valueOf(t.getLetter()));
+			}
+		}
+	 }
+	
+	private class GameTileSelectedListener implements EventHandler<MouseEvent> {
+		public void handle(MouseEvent me) {
+			playedTiles.append(tilesDisplayed.getSelectionModel().selectedItemProperty().get());
+			try {
+				tiles.remove(tilesDisplayed.getSelectionModel().selectedItemProperty().get());
+			} catch (TileNotInGroupException e) {
+				return;
+			}
+			tilesDisplayed.getSelectionModel().clearSelection();
+		}
+	}
+	
+	
+	private class YourTileSelectedListener implements EventHandler<MouseEvent> {
+		public void handle(MouseEvent me) {
+			tiles.append(yourWordDisplayed.getSelectionModel().selectedItemProperty().get());
+			try {
+				playedTiles.remove(yourWordDisplayed.getSelectionModel().selectedItemProperty().get());
+			} catch (TileNotInGroupException e) {
+				return;
+			}
+			yourWordDisplayed.getSelectionModel().clearSelection();
+			tilesDisplayed.setItems(tiles.tiles());
+		}
+	}
+	
+	
+	private class ResetListener implements EventHandler<MouseEvent> {
+		public void handle(MouseEvent me) {
+			for (Tile current : playedTiles.tiles()) {
+				tiles.append(current);
+			}
+			tilesDisplayed.setItems(tiles.tiles());
+			playedTiles.clear();
+		}
+	}
+	
+
+	
+
+	private class PlayWordListener  implements EventHandler<MouseEvent> {
+		@Override
+		public void handle(MouseEvent me) {
+			if (wordChecker.isValidWord(playedTiles.getHand())) {
+				runningScore += playedTiles.getScore();
+				playedTiles.clear();
+				
+				IntegerProperty integerProperty = new SimpleIntegerProperty(runningScore);
+				scoreField.textProperty().bindBidirectional(integerProperty, new NumberStringConverter());
+				
+				try {
+					BabbleController.this.fillTiles();
+				} catch (TileRackFullException | EmptyTileBagException e) {
+					return;
+				}
+
+			} else {
+				Alert a = new Alert(AlertType.INFORMATION);
+				a.setHeaderText("Error");
+				a.setContentText("Invalid Word");
+				a.show();
+
+			}
+		}
+	}
+
+	
 
 	public void fillTiles() throws TileRackFullException, EmptyTileBagException {
 		int tilesNeeded = tiles.getNumberOfTilesNeeded();
@@ -213,4 +175,27 @@ public class BabbleController implements Initializable {
 		tilesDisplayed.setItems(tiles.tiles());
 
 	}
+	
+	private void buildYourTilesDisplayed() {
+		yourWordDisplayed.setItems(playedTiles.tiles());
+		 this.yourWordDisplayed.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+			@Override
+			public ListCell<Tile> call(ListView<Tile> arg0) {
+				return new TileListCell();
+			}
+		 });
+		
+	}
+	
+	
+	private void buildGameTilesDisplayed() {
+		this.tilesDisplayed.setItems(tiles.tiles());
+		this.tilesDisplayed.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+			@Override
+			public ListCell<Tile> call(ListView<Tile> arg0) {
+				return new TileListCell();
+			}
+		 });
+	}
+	
 }
